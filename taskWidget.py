77308ac8fae1066a7a50a3cdd03b3aa64b542f89
hunterdyar <bloop@hdyar.com>
2023-09-25 14:08:24 -0400

    tinkering

--- taskWidget.py
+++ taskWidget.py
@@ -3,38 +3,42 @@ from threading import Thread
 from time import sleep
 
 from pytermgui import Container, StyleManager, real_length, tim
-
-
 from pytermgui import MouseEvent, Widget
 
 
 class Task(Widget):
     selected: bool = False
-    label: str = "No action"
+    completed: bool = False
+    task: str = "Some Task"
+    val: str = tim.parse("[inverse]" + "[ ]" + task + "[/inverse]")
 
-    def get_lines(self) -> list[str]:
+    def refresh_macro_output(self):
+        completedStr = "[x]" if self.completed else "[ ]"
         if self.selected:
-            return [tim.parse("[inverse]"+self.label+"[/inverse]")]
+            self.val = "[inverse]" + completedStr + " " + self.task + "[/inverse]"
         else:
-            return [self.label]
+            self.val = completedStr + " " + self.task
+    def get_lines(self) -> list[str]:
+        #self.refresh_macro_output()
+        completedStr = "[x]" if self.completed else "[ ]"
+        if self.selected:
+            self.val = "[inverse]" + completedStr + " " + self.task + "[/inverse]"
+        else:
+            self.val = completedStr + " " + self.task
+        return [tim.parse(self.val)]
 
     def on_left_click(self, event: MouseEvent) -> bool:
-        self.label = "Left click"
-
+        self.toggle()
+        self.refresh_macro_output()
         return True
 
-    def on_click(self, event: MouseEvent) -> bool:
-        self.label = "Generic click"
-
-        return True
-
-    def handle_mouse(self, event: MouseEvent) -> bool:
-        # Make sure we call the super handler
-        if super().handle_mouse(event):
-            return True
-
-        self.label = "No action"
+    def handle_key(self, key: str) -> bool:
         return True
 
     def highlight(self,sel: bool):
         self.selected=sel
+        self.refresh_macro_output()
+
+    def toggle(self):
+        self.completed = not self.completed
+        self.refresh_macro_output()
